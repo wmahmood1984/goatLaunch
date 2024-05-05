dee20aeb00d526783718bee618b83b5932e0d677
wmahmood1984 <56361582+wmahmood1984@users.noreply.github.com>
2024-05-05 11:44:27 +0500

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Router>
+        <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+        </Web3ReactProvider></Router>
+  
   </React.StrictMode>,
 )
